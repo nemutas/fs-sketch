59695140c4760cdd04db03a0826f9e031b6884c6
nemutas <inohiro.focru@gmail.com>
2024-05-01 13:40:07 +0900

    init

---
+++ src/shaders/temp.fs
@@ -0,0 +1,25 @@
+#version 300 es
+precision highp float;
+
+uniform sampler2D backBuffer;
+uniform vec2 resolution;
+uniform vec2 mouse;
+uniform float time;
+uniform float prevTime;
+uniform int frame;
+// uniform sampler2D textureUnit;
+// uniform samplerCube cubeTextureUnit;
+
+in vec2 vUv;
+out vec4 outColor;
+
+const float SEED = SEED_VALUE;
+
+void main() {
+  vec2 uv = vUv, asp = resolution / min(resolution.x, resolution.y);
+  outColor = vec4(uv, 0.0, 1.0);
+}
+
+//----------------------------
+// Reference
+//----------------------------
--- src/shaders/temp_output.fs
+++ src/shaders/temp_output.fs
@@ -1,23 +1,22 @@
 #version 300 es
 precision highp float;
 
-uniform sampler2D backBuffer;
+uniform sampler2D source;
 uniform vec2 resolution;
 uniform vec2 mouse;
 uniform float time;
-uniform float prevTime;
 uniform int frame;
 
 in vec2 vUv;
 out vec4 outColor;
 
-const float SEED = SEED_VALUE;
-
 void main() {
   vec2 uv = vUv, asp = resolution / min(resolution.x, resolution.y);
-  outColor = vec4(uv, 0.0, 1.0);
-}
 
-//----------------------------
-// Reference
-//----------------------------
+  float n = 20.0;
+  uv = floor(uv * asp * n) / asp / n;
+  vec3 source = texture(source, uv).rgb;
+  float gray = dot(source, vec3(0.333333));
+  
+  outColor = vec4(vec3(gray), 1.0);
+}
